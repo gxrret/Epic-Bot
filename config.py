248ebe724a64750367497dc48f5fbcb6599e49d0
Garret Koontz <koontzgarret@gmail.com>
2018-12-18 20:39:40 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = "epicness_bot"
-password = "gstro5678"
-client_id = "TEmlpeQ2Zxcd9w"
-client_secret = "FFGWyJ_-xqP2r0a7EdY5He8Ei3Q"
+username = "REDDIT_USERNAME"
+password = "REDDIT_PASSWORD"
+client_id = "YOUR_CLIENT_ID"
+client_secret = "YOUR_CLIENT_SECRET"
